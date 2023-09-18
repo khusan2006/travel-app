a606dbb9cd2db100db5cd50110ca5ad53c2fc244
khusan2006 <bahodirovkhusan@gmail.com>
2023-09-18 22:00:57 +0500

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Form from "./components/Form";
+import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
 
 function App() {
+  const [items, setItems] = useState([])
+  
+  const handleDeleteItem = (id) => {
+    setItems(items => items.filter(item => item.id !== id))
+  }
+
+  const handleToggleItem = (id) => {
+    setItems((items) => (
+      items.map((item) => {
+        return item.id === id ? {...item, packed: !item.packed} : item
+      })
+    ))
+  } 
+  const handleClearItems = () => {
+    const confirmed = window.confirm('Are you sure that you want delete all items')
+
+    confirmed && setItems([])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form setItems={setItems} />
+      <PackingList items={items} handleDeleteItem={handleDeleteItem} handleToggleItem={handleToggleItem} handleClearItems={handleClearItems}  />
+      <Stats items={items}/>
     </div>
   );
 }
